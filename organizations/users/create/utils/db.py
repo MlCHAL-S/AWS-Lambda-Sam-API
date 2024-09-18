72828e2149da5469c4115ade064379cd82590884
Michal <michal.szynkaruk@wp.pl>
2024-09-18 13:37:29 +0200

    Correcting a typo in db.py

--- organizations/users/create/utils/db.py
+++ organizations/users/create/utils/db.py
@@ -7,7 +7,7 @@ class MongoDBConnection:
 
     def __enter__(self):
         self.connection = MongoClient('mongodb+srv://nahciahprah:BUd2NgkyAm6h19vO@mydb.cd8zs.mongodb.net/?retryWrites=true&w=majority&appName=myDB')
-        return self.connection
+        return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         self.connection.close()
